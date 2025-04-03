0b4e1fe0117a2395e00ecfe37903857aa0ac8cd8
Taylor Brown <taylor@youneedabudget.com>
2025-04-03 11:34:14 -0500

    Rename and fix up asciinema3 formula

--- Formula/asciinema3.rb
+++ Formula/asciinema3.rb
@@ -1,5 +1,5 @@
-class AsciinemaRC3 < Formula
-  desc "Terminal session recorder"
+class Asciinema3 < Formula
+  desc "Terminal session recorder. Unofficial release for version 3.0.0-rc.3"
   homepage "https://asciinema.org"
   url "https://github.com/asciinema/asciinema.git",
       branch: "develop"
@@ -13,14 +13,17 @@ class AsciinemaRC3 < Formula
     # We do this because cargo install was giving me errors about Mac not being able to scan the binary - who knows if that is the right way to handle this?
     system "cargo", "build", "--release", "--locked"
     
-    # Manually copy the binary to the bin directory
-    bin.install "target/release/asciinema"    
+    # Manually copy the binary to the bin directory and name it "asciinema3"
+    bin.install "target/release/asciinema" => "asciinema3"
+
+    # Tell the user this is unofficial
+    ohai "Note: This is an unofficial Homebrew formula for asciinema version #{version}. It's been installed as asciinema3 to minimize confusion."
   end
 
   test do
     ENV["LC_ALL"] = "en_US.UTF-8"
-    output = shell_output("#{bin}/asciinema --version 2>&1")
-    assert_match "asciinema #{version}", output.strip,
+    output = shell_output("#{bin}/asciinema3 --version 2>&1")
+    assert_match "asciinema #{version}", output.strip
   end
 end
 
